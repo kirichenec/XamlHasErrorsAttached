028bdc6a765f9879907088f66c28dc33e3e99b74
kirichenec <kirichenecigor@gmail.com>
2019-12-01 11:47:31 +0300

    Add project files.

---
+++ XamlHasErrorsAttached/HasErrorsAttached.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace XamlHasErrorsAttached
+{
+    public static class HasErrorsAttached
+    {
+        public const string ErrorsList = nameof(ErrorsList);
+
+        public const string HasErrors = nameof(HasErrors);
+
+        #region ErrorsList
+        public static IList<ValidationError> GetErrorsList(DependencyObject obj)
+        {
+            return (IList<ValidationError>)obj.GetValue(ErrorsListProperty);
+        }
+
+        public static void SetErrorsList(DependencyObject obj, IList<ValidationError> value)
+        {
+            obj.SetValue(ErrorsListProperty, value);
+        }
+
+        public static readonly DependencyProperty ErrorsListProperty =
+            DependencyProperty.RegisterAttached(ErrorsList, typeof(IList<ValidationError>), typeof(HasErrorsAttached), new PropertyMetadata(new List<ValidationError>()));
+        #endregion
+
+        #region HasErrors
+        public static bool GetHasErrors(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(HasErrorsProperty);
+        }
+
+        public static void SetHasErrors(DependencyObject obj, bool value)
+        {
+            obj.SetValue(HasErrorsProperty, value);
+        }
+
+        public static readonly DependencyProperty HasErrorsProperty =
+            DependencyProperty.RegisterAttached(HasErrors, typeof(bool), typeof(HasErrorsAttached), new PropertyMetadata(false));
+        #endregion
+    }
+}
---
+++ XamlHasErrorsAttached/HasErrorsInitBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace XamlHasErrorsAttached
+{
+    public class HasErrorsInitBehavior : Behavior<FrameworkElement>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            Validation.AddErrorHandler(AssociatedObject, (s, args) =>
+            {
+                if (s is FrameworkElement element)
+                {
+                    var errorsList = HasErrorsAttached.GetErrorsList(element);
+                    if (errorsList == null)
+                    {
+                        throw new ArgumentException($"Attached {HasErrorsAttached.ErrorsList} is null");
+                    }
+
+                    if (args.Action == ValidationErrorEventAction.Added)
+                    {
+                        errorsList.Add(args.Error);
+                    }
+                    else
+                    {
+                        errorsList.Remove(args.Error);
+                    }
+                    HasErrorsAttached.SetHasErrors(element, errorsList.Any());
+                }
+            });
+        }
+    }
+}
