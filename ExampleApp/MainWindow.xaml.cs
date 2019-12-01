a106e855b35a57e834faf884136b6825d9e755bb
kirichenec <kirichenecigor@gmail.com>
2019-12-01 12:05:25 +0300

    [+] Example app [+] InverseBooleanConverter

---
+++ ExampleApp/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace TestValidation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = new MainWindowViewModel();
+        }
+    }
+}
---
+++ ExampleApp/MainWindowViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TestValidation
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private int _numberOne;
+
+        public int NumberOne
+        {
+            get { return _numberOne; }
+            set
+            {
+                if (_numberOne == value)
+                {
+                    return;
+                }
+                _numberOne = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _numberTwo;
+
+        public int NumberTwo
+        {
+            get { return _numberTwo; }
+            set
+            {
+                if (_numberTwo == value)
+                {
+                    return;
+                }
+                _numberTwo = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _numberThree;
+
+        public int NumberThree
+        {
+            get { return _numberThree; }
+            set
+            {
+                if (_numberThree == value)
+                {
+                    return;
+                }
+                _numberThree = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
