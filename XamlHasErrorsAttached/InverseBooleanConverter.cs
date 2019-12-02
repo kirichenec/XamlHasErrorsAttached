2b723127d50bc731c28dc596b6051847fdbcf89d
kirichenec <kirichenecigor@gmail.com>
2019-12-02 17:54:02 +0300

    [~] InverseBoolean method extracted

--- XamlHasErrorsAttached/InverseBooleanConverter.cs
+++ XamlHasErrorsAttached/InverseBooleanConverter.cs
@@ -9,13 +9,15 @@ namespace XamlHasErrorsAttached
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (targetType != typeof(bool))
-                throw new InvalidOperationException("The target must be a boolean");
-
-            return !(bool)value;
+            return InverseBoolean(value, targetType);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return InverseBoolean(value, targetType);
+        }
+
+        private static object InverseBoolean(object value, Type targetType)
         {
             if (targetType != typeof(bool))
                 throw new InvalidOperationException("The target must be a boolean");
